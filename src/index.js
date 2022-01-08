d1fd859dee2edb845d26aaf2bbb09efe1bd8c68c
MaxLabLearn <maxlablearn@gmail.com>
2022-01-08 23:11:00 +0300

    feat: emplement function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function reverse (n) {
-  
+    let strIn = '';
+    let strOut = '';
+    if (n > 0) {
+        strIn += n;
+    } else {
+        strIn += -n;
+    }; 
+    for (let i = strIn.length-1; i >= 0; i --) {
+        strOut += strIn[i];
+    }
+    return + strOut
 }
